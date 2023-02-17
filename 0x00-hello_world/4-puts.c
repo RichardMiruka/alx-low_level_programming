34877882d1ef1203487168ff5cd6e785c545bf87
richard-coder-oss <richardonsare15@gmail.com>
2023-02-17 04:12:14 -0800

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/* 
+   This program demonstrates the use of the puts() function to print a string to the console. 
+   The main() function does not take any arguments and returns an integer value. 
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
