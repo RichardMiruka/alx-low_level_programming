02f1e1996a83a49caf3d403bf86011d044bca9fa
richard-coder-oss <richardonsare15@gmail.com>
2023-02-17 03:58:02 -0800

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-  return (0);
+return (0);
 }
+
 
