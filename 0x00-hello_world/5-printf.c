636d9c85b39d179e6535dc3b526c315a85cd4d3b
richard-coder-oss <richardonsare15@gmail.com>
2023-02-17 05:06:37 -0800

    Using the function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ *main - the containing function
+ *
+ *Return 0 if successful or 1 if failed
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
