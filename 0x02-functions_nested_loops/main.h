55d375bc610dcde3338f6b6bb509ca2a4cdbcfab
richard-coder-oss <richardonsare15@gmail.com>
2023-02-21 23:25:58 -0800

    corrected header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,35 @@
-#ifndef Main_h
-#define main_h
+#ifndef MAIN_H_INCLUDED
 
-#include <stdio.h>
-#include <unistd.h>
+#define MAIN_H_INCLUDED
 
-int_putchar(char c);
+
+
+int _putchar(char c);
+
 void print_alphabet(void);
+
 void print_alphabet_x10(void);
+
+int _islower(int c);
+
 int _isalpha(int c);
+
 int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
 void jack_bauer(void);
+
 void times_table(void);
-int add(int, int);
+
+int add(int a, int b);
+
 void print_to_98(int n);
+
 void print_times_table(int n);
+
+
 
 #endif
