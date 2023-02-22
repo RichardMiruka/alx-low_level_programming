3b41873fd5c92c151be19d4c1dae4648eadec8a6
richard-coder-oss <richardonsare15@gmail.com>
2023-02-21 23:15:39 -0800

    header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef Main_h
+#defline main_h
+
+#include <stdio.h>
+#include <unistd.h>
+
+int_putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
